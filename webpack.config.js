5190bc2283bb6c09075205510608b0047760dae0
Vadim Abramov <vadimabramov322@gmail.com>
2023-03-11 18:05:52 +0300

    add webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    index: './src/index.tsx',
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
+
+if (isProd) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 3000,
+    liveReload: true,
+    open: true,
+    hot: true,
+    compress: true,
+  };
+}
+
+module.exports = config;
